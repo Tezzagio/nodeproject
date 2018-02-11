e2f8c9fe7cd670fc0c3bc8df5b59c592e7cd4764
Tezzagio <tezorobert@hotmail.com>
2018-02-11 13:43:25 +0000

    Adding new package.json file

--- nodeproject/app.js
+++ nodeproject/app.js
@@ -7,6 +7,10 @@ app.get('/', function(req, res){
     res.send('Aloha World!')
 });
 
+app.get('/routing', function(req, res){
+    res.send('Aloha Routing!')
+});
+
     app.listen(port, function(err){
         console.log('The server is running on port: ' + port)
 });
