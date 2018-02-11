b71f18723a6f4dd4b9b88e78f5da42a7e56b8006
Tezzagio <tezorobert@hotmail.com>
2018-02-11 13:13:46 +0000

    Second commit

--- nodeproject/app.js
+++ nodeproject/app.js
@@ -1,2 +1,14 @@
-var aloha = 'Aloha Variables';
-console.log(aloha);
+var express = require('express');
+var app = express();
+
+var port = process.env.PORT;
+
+app.get('/', function(req, res){
+    res.send('Aloha World!')
+});
+
+    app.listen(port, function(err){
+        console.log('The server is running on port: ' + port)
+});
+
+
