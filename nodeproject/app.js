fa12d090306d0ccfbd3acc564e0fd8ae3e481c38
Tezzagio <tezorobert@hotmail.com>
2018-02-19 17:45:21 +0000

    Changes made to Public folder

--- nodeproject/app.js
+++ nodeproject/app.js
@@ -4,6 +4,8 @@ var app = express();
 var port = process.env.PORT;
 
 app.use(express.static('public'));
+app.use(express.static('src/views'));
+app.use(express.static('bower_components'));
 
 app.get('/', function(req, res){
     res.send('Tezzagio World!')
@@ -13,6 +15,6 @@ app.get('/routing', function(req, res){
     res.send('Tezzagio Routing!')
 });
 
-    app.listen(port, function(err){
-        console.log('The server is running on port: ' + port)
+app.listen(port, function(err){
+    console.log('The server is running on port: ' + port)
 });
